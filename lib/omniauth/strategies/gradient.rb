950c40a5639d74194b217d73c88e61bd325070e8
David Stuckmann <david@retireup.com>
2018-12-04 09:21:58 -0600

    Add call to userinfo endpoint

--- lib/omniauth/strategies/gradient.rb
+++ lib/omniauth/strategies/gradient.rb
@@ -10,6 +10,26 @@ module OmniAuth
           authorize_url: 'https://loginalpha.konnexme.com/identity/connect/authorize',
           token_url: 'https://loginalpha.konnexme.com/identity/connect/token'
       }
+
+      # uid{ raw_info['UserId'] }
+
+      # info do
+      #   {
+      #     :first_name => raw_info['FirstName'],
+      #     :last_name => raw_info['LastName'],
+      #     :email => raw_info['EmailAddress']
+      #   }
+      # end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/userinfo').parsed
+      end
     end
   end
 end

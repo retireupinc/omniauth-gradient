a98372fff61466e3f9bcfebddd9ab3014bef5163
David Stuckmann <david@retireup.com>
2018-12-04 16:56:16 -0600

    Provide dummy user info for now

--- lib/omniauth/strategies/gradient.rb
+++ lib/omniauth/strategies/gradient.rb
@@ -11,15 +11,16 @@ module OmniAuth
           token_url: 'https://loginalpha.konnexme.com/identity/connect/token'
       }
 
-      # uid{ raw_info['UserId'] }
+      uid{ raw_info['UserId'] }
 
-      # info do
-      #   {
-      #     :first_name => raw_info['FirstName'],
-      #     :last_name => raw_info['LastName'],
-      #     :email => raw_info['EmailAddress']
-      #   }
-      # end
+      info do
+        {
+          :name => "#{raw_info['FirstName']} #{raw_info['LastName']}",
+          :first_name => raw_info['FirstName'],
+          :last_name => raw_info['LastName'],
+          :email => raw_info['EmailAddress']
+        }
+      end
 
       extra do
         {
@@ -28,7 +29,13 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/clients?searchString=smith', ).parsed
+        # @raw_info ||= access_token.get('/me', ).parsed
+        @raw_info || = {
+          UserId: '999999999999',
+          EmailAddress: 'rupadvisor@email.com',
+          FirstName: 'rup',
+          LastName: 'Advisor'
+        }
       end
     end
   end
